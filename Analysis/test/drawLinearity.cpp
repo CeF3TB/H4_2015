6713decb4e92b812ca0da5585a18da2c418be69f
Francesco <micheli@cern.ch>
2015-07-08 15:30:04 +0200

    adding skeleton for linearity analysis

---
+++ Analysis/test/drawLinearity.cpp
@@ -0,0 +1,40 @@
+#define fastPlotter_cxx
+#include "LinearityLooper.h"
+#include "DrawTools.h"
+#include <TH2.h>
+#include <TStyle.h>
+#include <TCanvas.h>
+
+
+std::vector<std::string> runs;
+std::vector<float> beamEnergy;
+
+
+int main( int argc, char* argv[] ) {
+
+  DrawTools::setStyle();
+
+  TFile *inputFile;
+
+  if( argc>1 ) {
+    std::string filename_str(argv[1]);
+    inputFile=TFile::Open(filename_str.c_str());
+    
+    if( inputFile==0 ) {
+      std::cout << "ERROR! Din't find file " << filename_str << std::endl;
+      std::cout << "Exiting." << std::endl;
+      exit(11);
+    }
+    
+    
+  }
+
+
+  TTree* inputTree=(TTree*)inputFile->Get("outputTree");
+
+  LinearityLooper t(inputTree);
+  t.Loop();
+
+  return 0;
+
+}
