1619585d9422b555db06ba0369a05bcd72bb771b
Francesco <micheli@cern.ch>
2015-07-20 16:27:48 +0200

    small fix

--- Analysis/test/makeAnalysisTree.cpp
+++ Analysis/test/makeAnalysisTree.cpp
@@ -58,7 +58,7 @@ int main( int argc, char* argv[] ) {
    } else {
 
      std::cout << "Usage:" << std::endl;
-     std::cout << "./makeAnalysisTree [runName]" << std::endl;
+     std::cout << "./makeAnalysisTree [runName] ([tag])" << std::endl;
      exit(12345);
 
    }
