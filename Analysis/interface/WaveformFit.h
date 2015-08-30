fefecf2ee179418485822d2610b4af3168f1da26
Francesco <micheli@cern.ch>
2015-08-30 14:01:10 +0200

    working version of waveform fit

---
+++ Analysis/interface/WaveformFit.h
@@ -0,0 +1,15 @@
+#ifndef WAVEFORM_FIT_H
+#define WAVEFORM_FIT_H
+
+#include "TProfile.h"
+#include "interface/Waveform.h"
+#include "Math/Minimizer.h"
+#include "interface/WaveformFit.h"
+
+namespace WaveformFit
+{
+  void alignWaveform(TProfile* ref_profile, TProfile* fit_profile, ROOT::Math::Minimizer* &minimizer);
+  void fitWaveform(Waveform* wave, TProfile* amplitudeProfile,  int samplesBeforeMax, int samplesAfterMax, const Waveform::max_amplitude_informations& max, const Waveform::baseline_informations& rms, ROOT::Math::Minimizer* &minimizer);
+  void fitWaveformSimple(Waveform* wave, TProfile* amplitudeProfile,  int samplesBeforeMax, int samplesAfterMax, const Waveform::max_amplitude_informations& max, const Waveform::baseline_informations& rms, ROOT::Math::Minimizer* &minimizer);
+}
+#endif
