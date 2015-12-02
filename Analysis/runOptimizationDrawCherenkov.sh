9933ebebbbf486a576cc3ee857f6c3ac81818b38
Francesco <micheli@cern.ch>
2015-12-02 12:34:22 +0100

    script to run Optimization

---
+++ Analysis/runOptimizationDrawCherenkov.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+run=$1
+echo "running run $run"
+for i in {85..90..5}
+do
+    ./bin/drawCherenkov $run V10 $i
+    mkdir plots_drawCherenkov_opt/
+    mkdir plots_drawCherenkov_opt/$run/
+    mkdir plots_drawCherenkov_opt/$run/$i
+    cp plots_drawCherenkov/*$run* plots_drawCherenkov_opt/$run/$i/
+done
+
+./bin/drawCherenkov $run V10
