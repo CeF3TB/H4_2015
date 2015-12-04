04306741838a188b444e3b0f5e2018f45bf22d63
Francesco <micheli@cern.ch>
2015-12-04 12:35:43 +0100

    small fix

--- Analysis/runOptimizationWindow_1.sh
+++ Analysis/runOptimizationWindow_1.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 run=$1
+echo "running run $run"
 
-for i in {85..90..5}
+for i in {85..115..5}
 do 
     cp config/PMTEarlyOctober2015Config_optimization.xml config/PMTEarlyOctober2015Config_optimization_$i.xml
     sed -i "s|startSample>105|startSample>$i|" config/PMTEarlyOctober2015Config_optimization_$i.xml
     sed -i "s|optStart|optStart_$i|" config/PMTEarlyOctober2015Config_optimization_$i.xml
-#    ./bin/makeAnalysisTree $run V10 PMTEarlyOctober2015Config_optimization_$i; 
+    ./bin/makeAnalysisTree $run V10 PMTEarlyOctober2015Config_optimization_$i; 
 done
