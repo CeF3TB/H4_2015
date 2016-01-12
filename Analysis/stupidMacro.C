1affdc10b8d84f10ae5cc0cf22ed66115fc40f71
michelif <francesco.micheli@cern.ch>
2016-01-12 10:24:26 +0100

    small fix

--- Analysis/stupidMacro.C
+++ Analysis/stupidMacro.C
@@ -9,12 +9,12 @@
 
   recoTree->Draw("wc_y_corr>>h2(80,-20.5,19.5)","cef3_time_at_thresh[1]>0 && cef3_time_at_thresh[1]<50 && mcp_max_amplitude>200 && cef3_maxAmpl[2]>100 && TMath::Abs(cef3_time_at_frac50[1]-mcp_time_frac50)<11 && wc_x>-20 && (cef3_time_at_frac50[1]-mcp_time_frac50)>7 && (cef3_time_at_frac50[1]-mcp_time_frac50)<8.6","");
   c1->SaveAs("plots_timing_posOpt/y_lower.pdf");
-  recoTree->Draw("wc_y_corr>>h(80,-20.5,19.5)","cef3_time_at_thresh[1]>0 && cef3_time_at_thresh[1]<50 && mcp_max_amplitude>200 && cef3_maxAmpl[2]>100 && TMath::Abs(cef3_time_at_frac50[1]-mcp_time_frac50)<11 && wc_x>-20 && (cef3_time_at_frac50[1]-mcp_time_frac50)>7 && (cef3_time_at_frac50[1]-mcp_time_frac50)>8.6","")
+  recoTree->Draw("wc_y_corr>>h2(80,-20.5,19.5)","cef3_time_at_thresh[1]>0 && cef3_time_at_thresh[1]<50 && mcp_max_amplitude>200 && cef3_maxAmpl[2]>100 && TMath::Abs(cef3_time_at_frac50[1]-mcp_time_frac50)<11 && wc_x>-20 && (cef3_time_at_frac50[1]-mcp_time_frac50)>7 && (cef3_time_at_frac50[1]-mcp_time_frac50)>8.6","");
  c1->SaveAs("plots_timing_posOpt/y_upper.pdf");
 
   recoTree->Draw("wc_x_corr>>h2(80,-20.5,19.5)","cef3_time_at_thresh[1]>0 && cef3_time_at_thresh[1]<50 && mcp_max_amplitude>200 && cef3_maxAmpl[2]>100 && TMath::Abs(cef3_time_at_frac50[1]-mcp_time_frac50)<11 && wc_x>-20 && (cef3_time_at_frac50[1]-mcp_time_frac50)>7 && (cef3_time_at_frac50[1]-mcp_time_frac50)<8.6","");
   c1->SaveAs("plots_timing_posOpt/x_lower.pdf");
-  recoTree->Draw("wc_x_corr>>h(80,-20.5,19.5)","cef3_time_at_thresh[1]>0 && cef3_time_at_thresh[1]<50 && mcp_max_amplitude>200 && cef3_maxAmpl[2]>100 && TMath::Abs(cef3_time_at_frac50[1]-mcp_time_frac50)<11 && wc_x>-20 && (cef3_time_at_frac50[1]-mcp_time_frac50)>7 && (cef3_time_at_frac50[1]-mcp_time_frac50)>8.6","");
+  recoTree->Draw("wc_x_corr>>h2(80,-20.5,19.5)","cef3_time_at_thresh[1]>0 && cef3_time_at_thresh[1]<50 && mcp_max_amplitude>200 && cef3_maxAmpl[2]>100 && TMath::Abs(cef3_time_at_frac50[1]-mcp_time_frac50)<11 && wc_x>-20 && (cef3_time_at_frac50[1]-mcp_time_frac50)>7 && (cef3_time_at_frac50[1]-mcp_time_frac50)>8.6","");
  c1->SaveAs("plots_timing_posOpt/x_upper.pdf");
 
   for (int i=0;i<10;++i){
