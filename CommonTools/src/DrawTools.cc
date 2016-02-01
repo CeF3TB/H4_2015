341ea6dca8d7b275b64e09d1a3c0291f443bc4aa
Francesco <micheli@cern.ch>
2016-02-01 12:17:32 +0100

    adding pretty palette for 2d plots

--- CommonTools/src/DrawTools.cc
+++ CommonTools/src/DrawTools.cc
@@ -65,6 +65,21 @@ TStyle* DrawTools::setStyle() {
   style->SetPadBottomMargin(0.12);
   style->SetPadLeftMargin(0.12);
   style->SetPalette(1,0);
+
+///////// pretty palette ///////////
+
+  const Int_t NRGBs = 5;
+  const Int_t NCont = 255;
+  
+  Double_t stops[NRGBs] = { 0.00, 0.34, 0.61, 0.84, 1.00 };
+  Double_t red[NRGBs]   = { 0.00, 0.00, 0.87, 1.00, 0.51 };
+  Double_t green[NRGBs] = { 0.00, 0.81, 1.00, 0.20, 0.00 };
+  Double_t blue[NRGBs]  = { 0.51, 1.00, 0.12, 0.00, 0.00 };
+  TColor::CreateGradientColorTable(NRGBs, stops, red, green, blue, NCont);
+  style->SetNumberContours(NCont);
+
+
+
   style->cd();
 
   //for the histos
