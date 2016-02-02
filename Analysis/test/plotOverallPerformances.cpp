25ecb8f1c25fce50320083d830f9f0ee748346ad
Francesco <micheli@cern.ch>
2016-02-02 18:48:13 +0100

    adding plots and nino sel

--- Analysis/test/plotOverallPerformances.cpp
+++ Analysis/test/plotOverallPerformances.cpp
@@ -65,24 +65,33 @@ int main( int argc, char* argv[] ) {
   
   TH1F* reso_histo_fibre_corr_Amplitude[theConfiguration_.nMaxAmplCuts];//to see behaviour of resolution as a function of ampl
   TH1F* reso_histo_channel_corr_Amplitude[theConfiguration_.nMaxAmplCuts];
+
+  TH1F* reso_histo_fibre_total;//all events at different energies all together with reasonable cut on amplitude
+  TH1F* reso_histo_channel_total;
+
   TVectorD resValueAmplitude_fibre(theConfiguration_.nMaxAmplCuts);
   TVectorD resErrValueAmplitude_fibre(theConfiguration_.nMaxAmplCuts);
   
   TVectorD resValueAmplitude_channel(theConfiguration_.nMaxAmplCuts);
   TVectorD resErrValueAmplitude_channel(theConfiguration_.nMaxAmplCuts);
+  //  TVectorD resValueAmplitudeSingleEnergy_channel(theConfiguration_.runs.size());
+  //  TVectorD resValueAmplitudeSingleEnergy_fibre(theConfiguration_.runs.size());
+
 
 
    for (int i=0;i<theConfiguration_.nMaxAmplCuts;++i){
     TString icut;
     icut.Form("%d",i); 
     reso_histo_fibre_corr_Amplitude[i] = new TH1F("reso_histo_fibre_corr_Amplitude_"+icut,"reso_histo_fibre_corr_Amplitude_"+icut,200,theConfiguration_.rangeXLow,theConfiguration_.rangeXUp);
-
     reso_histo_channel_corr_Amplitude[i] = new TH1F("reso_histo_channel_corr_Amplitude_"+icut,"reso_histo_channel_corr_Amplitude_"+icut,200,theConfiguration_.rangeXLow,theConfiguration_.rangeXUp);
-
    }
-
-
-  std::vector<int> runs;
+   
+   reso_histo_fibre_total = new TH1F("reso_histo_fibre_total","reso_histo_fibre_total",200,theConfiguration_.rangeXLow,theConfiguration_.rangeXUp);
+   reso_histo_channel_total = new TH1F("reso_histo_channel_total","reso_histo_channel_total",200,theConfiguration_.rangeXLow,theConfiguration_.rangeXUp);
+   
+   
+   std::vector<int> runs;
+   std::vector<int> energies;
 
   TString haddFile = "analysisTrees_"+tag+"/"+theConfiguration_.setup+"_runs.root";
 
@@ -99,6 +108,7 @@ int main( int argc, char* argv[] ) {
   for (int i=0;i<theConfiguration_.runs.size();++i){
     TString fileNameString = "analysisTrees_"+tag+"/Reco_";
     runs.push_back(theConfiguration_.runs[i]);
+    energies.push_back(theConfiguration_.energies[i]);
     fileNameString+=runs[i];
     haddString+=fileNameString;
     haddString+= ".root ";
@@ -126,12 +136,14 @@ int main( int argc, char* argv[] ) {
 	  if(t.cef3_maxAmpl->at(1)>theConfiguration_.startCutFibre+i*theConfiguration_.stepAmplFibre && t.cef3_maxAmpl->at(1)<theConfiguration_.startCutFibre+(i+1)*theConfiguration_.stepAmplFibre){
 
 	    reso_histo_fibre_corr_Amplitude[i]->Fill(t.cef3_time_at_frac50->at(1)-t.mcp_time_frac50);
+	    if(t.cef3_maxAmpl->at(1)>100)reso_histo_fibre_total->Fill(t.cef3_time_at_frac50->at(1)-t.mcp_time_frac50);
 	    break;
 	  }
       }
 	  if(passesChannelTopologicalSelection(t)  && t.cef3_maxAmpl->at(2) > theConfiguration_.channel2CutChannel*t.beamEnergy/lowerBeamEnergy && t.cef3_maxAmpl->at(1)<theConfiguration_.channel1CutChannel){   //cuts on fibre position with hodos and wc. cut on cef3_maxAmpl[2] to reduce hadron contamination, cef3_maxAmpl[1] cut to reduce remaining events hitting the fibre
 	    if(t.cef3_maxAmpl->at(1)>i*theConfiguration_.stepAmplChannel && t.cef3_maxAmpl->at(1)<(i+1)*theConfiguration_.stepAmplChannel){
 	    reso_histo_channel_corr_Amplitude[i]->Fill(t.cef3_time_at_frac50->at(1)-t.mcp_time_frac50);
+	    if(t.cef3_maxAmpl->at(1)>100)reso_histo_channel_total->Fill(t.cef3_time_at_frac50->at(1)-t.mcp_time_frac50);
 	    break;
 	    }
 	  }
@@ -201,8 +213,9 @@ int main( int argc, char* argv[] ) {
      TCanvas* cans = new TCanvas();
      cans->cd();
      frame->Draw();
-     TLegend* lego = new TLegend(0.6, 0.8, 0.8, 0.9);
+     TLegend* lego = new TLegend(0.57, 0.8, 0.89, 0.9);;
      lego->SetTextSize(0.036);
+     lego->SetTextAlign(32); // align right
      lego->AddEntry(  (TObject*)0 ,Form("#sigma = %.1f #pm %.1f ps", rms*1.e3, rmsErr*1.e3), "");
      
      lego->SetFillColor(0);
@@ -267,8 +280,9 @@ int main( int argc, char* argv[] ) {
        TCanvas* cans = new TCanvas();
        cans->cd();
        frame->Draw();
-       TLegend* lego = new TLegend(0.6, 0.8, 0.8, 0.9);
+       TLegend* lego = new TLegend(0.57, 0.8, 0.89, 0.9);;
        lego->SetTextSize(0.036);
+       lego->SetTextAlign(32); // align right
        lego->AddEntry(  (TObject*)0 ,Form("#sigma = %.1f #pm %.1f ps", rms*1.e3, rmsErr*1.e3), "");
      
        lego->SetFillColor(0);
@@ -290,6 +304,9 @@ int main( int argc, char* argv[] ) {
    //plot res vs amplitude
    TGraphErrors*   resVsAmplitude_fibre = new TGraphErrors(0);
    TGraphErrors*   resVsAmplitude_channel = new TGraphErrors(0);
+
+   resVsAmplitude_channel->SetName("resVsAmplitude_channel");
+   resVsAmplitude_fibre->SetName("resVsAmplitude_fibre");
    
    for(int i=0;i<resValueAmplitude_channel.GetNoElements();i++){
      if(resValueAmplitude_channel[i]!=0){ 
@@ -304,7 +321,10 @@ int main( int argc, char* argv[] ) {
    }
 	 
 	 
-   
+   std::string outFileName;
+   outFileName = dir+"/plotOverallPerformances_"+tag+".root";
+   TFile* outFile = TFile::Open(outFileName.c_str(),"recreate");
+
    
    TCanvas* c1 = new TCanvas( "c1", "", 600, 600 );
    float yup=1.1*(resVsAmplitude_channel->GetY()[0]+resVsAmplitude_channel->GetEY()[0]);
@@ -313,6 +333,13 @@ int main( int argc, char* argv[] ) {
 
 
    //channel   
+   resVsAmplitude_channel->SetName("resVsAmplitude_channel");
+   resVsAmplitude_channel->SetMarkerStyle(20);
+   resVsAmplitude_channel->SetMarkerSize(1.6);
+   resVsAmplitude_channel->SetMarkerColor(kBlue);
+
+
+   resVsAmplitude_channel->Write();
    h2_axes->SetYTitle("#sigma_{t} [ps]");
    h2_axes->GetXaxis()->SetTitle("Amplitude [ADC]");
    h2_axes->Draw(""); 
@@ -320,10 +347,6 @@ int main( int argc, char* argv[] ) {
    TF1* f= new TF1("fun","[1]/x+[0]",(resVsAmplitude_channel->GetX())[0]-10,(resVsAmplitude_channel->GetX())[resVsAmplitude_channel->GetN()-1] +10);
    resVsAmplitude_channel->Fit("fun");
 
-    resVsAmplitude_channel->SetName("resVsAmplitude");
-    resVsAmplitude_channel->SetMarkerStyle(20);
-    resVsAmplitude_channel->SetMarkerSize(1.6);
-    resVsAmplitude_channel->SetMarkerColor(kBlue);
     resVsAmplitude_channel->Draw("p same");
 
 
@@ -351,16 +374,17 @@ int main( int argc, char* argv[] ) {
 
 
    //fibre   
+   resVsAmplitude_fibre->SetName("resVsAmplitude_fibre");
+   resVsAmplitude_fibre->SetMarkerStyle(20);
+   resVsAmplitude_fibre->SetMarkerSize(1.6);
+   resVsAmplitude_fibre->SetMarkerColor(kBlue);
+   resVsAmplitude_fibre->Write();
    h2_axes_2->SetYTitle("#sigma_{t} [ps]");
    h2_axes_2->GetXaxis()->SetTitle("Amplitude [ADC]");
    h2_axes_2->Draw(""); 
 
    resVsAmplitude_fibre->Fit("fun");
 
-   resVsAmplitude_fibre->SetName("resVsAmplitude");
-   resVsAmplitude_fibre->SetMarkerStyle(20);
-   resVsAmplitude_fibre->SetMarkerSize(1.6);
-   resVsAmplitude_fibre->SetMarkerColor(kBlue);
    resVsAmplitude_fibre->Draw("p same");
    
 
@@ -378,8 +402,135 @@ int main( int argc, char* argv[] ) {
    
    c1->SaveAs(dir+"/timingResolutionVsAmplitudeSiPM_fibre.png");
    c1->SaveAs(dir+"/timingResolutionVsAmplitudeSiPM_fibre.pdf");  
-   
 
+
+   //fit histos for all energies with reasonable cut on ampl
+   //fibre
+   if(reso_histo_fibre_total->GetEntries()>=20){
+     TH1F* histo;
+     histo=reso_histo_fibre_total;
+     double peakpos = histo->GetBinCenter(histo->GetMaximumBin());
+     double sigma = histo->GetRMS();
+     
+     double fitmin;
+     double fitmax;
+  
+  
+     fitmin = peakpos-4*sigma;
+     fitmax = peakpos+4*sigma;
+  
+     RooRealVar x("x","deltaT", fitmin, fitmax);
+     RooDataHist data("data","dataset with x",x,RooFit::Import(*histo) );
+
+     RooRealVar meanr("meanr","Mean",peakpos,peakpos-3*sigma, peakpos+3*sigma);
+     RooRealVar widthL("widthL","#sigmaL",sigma , 0, 5*sigma);
+     RooRealVar widthR("widthR","#sigmaR",sigma , 0, 5*sigma);
+     RooRealVar alphaL("alphaL","#alpha",5.08615e-02 , 0., 1.);
+     RooRealVar alphaR("alphaR","#alpha",5.08615e-02, 0., 1.);
+     int ndf;
+     
+     RooPlot* frame;
+
+     RooCruijff fit_fct("fit_fct","fit_fct",x,meanr,widthL,widthR,alphaL,alphaR); ndf = 5;
+     fit_fct.fitTo(data);
+    
+     std::string ytitle = Form("Events");
+     frame = x.frame("Title");
+     frame->SetXTitle("time_{fibre}-time_{mcp} [ns]");
+     frame->SetYTitle(ytitle.c_str());
+     
+     data.plotOn(frame);  //this will show histogram data points on canvas 
+     fit_fct.plotOn(frame);//this will show fit overlay on canvas  
+
+     double rms,rmsErr;
+     rms = (widthL.getVal()+widthR.getVal())/2;
+     rmsErr = 0.5*sqrt(widthL.getError()*widthL.getError()+widthR.getError()*widthR.getError());
+
+     TCanvas* cans = new TCanvas();
+     cans->cd();
+     frame->Draw();
+     TLegend* lego = new TLegend(0.57, 0.8, 0.89, 0.9);
+     lego->SetTextAlign(32); // align right
+     lego->SetTextSize(0.036);
+     lego->AddEntry(  (TObject*)0 ,Form("#sigma = %.1f #pm %.1f ps", rms*1.e3, rmsErr*1.e3), "");
+     
+     lego->SetFillColor(0);
+     lego->Draw("same");
+     
+     TPaveText* pave = DrawTools::getLabelTop_expOnXaxis("Electron Beam");
+     pave->Draw("same");
+     
+
+     cans->SaveAs(dir+"/reso_histo_fibre_total.png");
+     cans->SaveAs(dir+"/reso_histo_fibre_total.pdf");
+
+     }
+
+     //channel
+     if(reso_histo_channel_total->GetEntries()>20){
+       TH1F* histo;
+       histo=reso_histo_channel_total;
+       double peakpos = histo->GetBinCenter(histo->GetMaximumBin());
+       double sigma = histo->GetRMS();
+       
+       double fitmin;
+       double fitmax;
+  
+  
+       fitmin = peakpos-4*sigma;
+       fitmax = peakpos+4*sigma;
+  
+       RooRealVar x("x","deltaT", fitmin, fitmax);
+       RooDataHist data("data","dataset with x",x,RooFit::Import(*histo) );
+
+       RooRealVar meanr("meanr","Mean",peakpos,peakpos-3*sigma, peakpos+3*sigma);
+       RooRealVar widthL("widthL","#sigmaL",sigma , 0, 5*sigma);
+       RooRealVar widthR("widthR","#sigmaR",sigma , 0, 5*sigma);
+       RooRealVar alphaL("alphaL","#alpha",5.08615e-02 , 0., 1.);
+       RooRealVar alphaR("alphaR","#alpha",5.08615e-02, 0., 1.);
+       int ndf;
+     
+       RooPlot* frame;
+
+       RooCruijff fit_fct("fit_fct","fit_fct",x,meanr,widthL,widthR,alphaL,alphaR); ndf = 5;
+       fit_fct.fitTo(data);
+    
+       std::string ytitle = Form("Events");
+       frame = x.frame("Title");
+       frame->SetXTitle("time_{fibre}-time_{mcp} [ns]");
+       frame->SetYTitle(ytitle.c_str());
+     
+       data.plotOn(frame);  //this will show histogram data points on canvas 
+       fit_fct.plotOn(frame);//this will show fit overlay on canvas  
+
+       double rms,rmsErr;
+       rms = (widthL.getVal()+widthR.getVal())/2;
+       rmsErr = 0.5*sqrt(widthL.getError()*widthL.getError()+widthR.getError()*widthR.getError());
+
+
+       TCanvas* cans = new TCanvas();
+       cans->cd();
+       frame->Draw();
+       TLegend* lego = new TLegend(0.57, 0.8, 0.89, 0.9);;
+       lego->SetTextSize(0.036);
+       lego->SetTextAlign(32); // align right
+       lego->AddEntry(  (TObject*)0 ,Form("#sigma = %.1f #pm %.1f ps", rms*1.e3, rmsErr*1.e3), "");
+     
+       lego->SetFillColor(0);
+       lego->Draw("same");
+     
+       TPaveText* pave = DrawTools::getLabelTop_expOnXaxis("Electron Beam");
+       pave->Draw("same");
+     
+       cans->SaveAs(dir+"/reso_histo_channel_total.png");
+       cans->SaveAs(dir+"/reso_histo_channel_total.pdf");
+     }
+
+
+
+   
+   outFile->Write();
+   outFile->Close();
 
   return 0;
 
@@ -419,6 +570,9 @@ plotOverallPerformances_Config_t readConfiguration(std::string configName){
    conf.addTagFileName= Configurator::GetInt(Configurable::getElementContent(*configurator_,"addTagFileName",configurator_->root_element));
 
    conf.tagFileName=Configurable::getElementContent (*configurator_, "tagFileName",configurator_->root_element) ;
+
+  content = Configurable::getElementContent (*configurator_, "energies",configurator_->root_element) ;
+  Configurator::GetVecFloat(content,conf.energies);
 
 
    return conf;
