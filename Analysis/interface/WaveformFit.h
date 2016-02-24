731cba3d4572efe410737660a02f1abdbc2fc81c
Francesco <micheli@cern.ch>
2016-02-24 19:49:24 +0100

    adding timeaboveThr

--- Analysis/interface/WaveformFit.h
+++ Analysis/interface/WaveformFit.h
@@ -13,6 +13,7 @@ namespace WaveformFit
   void fitWaveformSimple(Waveform* wave, TProfile* amplitudeProfile,  int samplesBeforeMax, int samplesAfterMax, const Waveform::max_amplitude_informations& max, const Waveform::baseline_informations& rms, ROOT::Math::Minimizer* &minimizer, bool fixedWindow=false, int minSample=0, int MaxSample=700);
   void fitWaveformSimplePlusTime(Waveform* wave, TProfile* amplitudeProfile,  int samplesBeforeMax, int samplesAfterMax, const Waveform::max_amplitude_informations& max, const Waveform::baseline_informations& rms, ROOT::Math::Minimizer* &minimizer, bool fixedWindow=false, int minSample=0, int MaxSample=700);
   std::pair<float, float> GetTimeLE(Waveform* wave,const Waveform::baseline_informations& waveRms,float thr, int nmFitSamples, int npFitSamples, int min, int max, float timeUnit);
+  float GetTimeAboveThr(Waveform* wave,float thr, int min, int max,float timeUnit);
   float LinearInterpolation(Waveform* wave,const Waveform::baseline_informations& waveRms, float& A, float& B, const int& min, const int& max, float timeUnit);
   std::pair<float,float> GetSignalIntegral(Waveform* wave,int thr, int min);
 }
